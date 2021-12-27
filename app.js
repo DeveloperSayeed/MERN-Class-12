f3feb3f0ff13d46aefcca1f355443c99e3d4f4de
Developersayeed <abusayeedriday@gmail.com>
2021-12-27 23:39:36 +0300

    add

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const ageYear = document.querySelector("#ageyear");
 const result = document.querySelector("#result");
 
 
+ageName.addEventListener("click", () => {
+    result.innerHTML = "";
+    button.innerHTML = "Check Your Result"
+})
+ageYear.addEventListener("click", () => {
+    result.innerHTML = "";
+    button.innerHTML = "Check Your Result"
+})
+
 button.addEventListener("click", () => {
 
     var ageName = document.getElementById('agename').value;
@@ -15,18 +24,23 @@ button.addEventListener("click", () => {
     var ageResult = yearRGEX.test(ageYear);
 
 
-  
-        if (nameResult && ageResult) {
-            result.innerHTML = ageCal(ageName, ageYear);
-        } else if(!nameResult && ageResult) {
-            result.innerHTML = `<p class="alert-danger p-4">Plase Write Your Valid  Name</p>`;
 
-        } else if(nameResult && !ageResult){
-            result.innerHTML = `<p class="alert-danger p-4">Plase Write Your Valid Date Of Birth</p>`;
-        }else{
-            result.innerHTML = `<p class="alert-danger p-4">Plase Write Your Valid Right Information</p>`;
-        }
- 
-    document.getElementById('agename').value = "";
-    document.getElementById('ageyear').value = "";
+    if (nameResult && ageResult ) {
+        result.innerHTML = ageCal(ageName, ageYear);
+        document.getElementById('agename').value = "";
+        document.getElementById('ageyear').value = "";
+        button.innerHTML="Done";
+    } else if (!nameResult && ageResult) {
+        result.innerHTML = `<p class="alert-danger p-4">Plase Write Your Valid  Name</p>`;
+
+
+    } else if (nameResult && !ageResult) {
+        result.innerHTML = `<p class="alert-danger p-4">Plase Write Your Valid Date Of Birth</p>`;
+
+    } else {
+        result.innerHTML = `<p class="alert-danger p-4">Plase Write Your Valid Right Information</p>`;
+    }
+
+    // document.getElementById('agename').value = "";
+    // document.getElementById('ageyear').value = "";
 });
